9bb5c15e10217ba9d621835dcc632a01ccbdb072
BMilliet <39925357+BMilliet@users.noreply.github.com>
2019-10-16 18:09:02 -0300

    Add files via upload

---
+++ xcode_cache_clean.sh
@@ -0,0 +1,8 @@
+echo "deleting CoreSimulator/Devices..."
+rm -rf ~/Library/Developer/CoreSimulator/Devices/*
+echo "deleting Xcode/DerivedData..."
+rm -rf ~/Library/Developer/Xcode/DerivedData/*
+echo "deleting Xcode/iOS\ DeviceSupport..."
+rm -rf ~/Library/Developer/Xcode/iOS\ DeviceSupport/*
+echo "deleting Xcode/iOS\ Device\ Logs..."
+rm -rf ~/Library/Developer/Xcode/iOS\ Device\ Logs/*
